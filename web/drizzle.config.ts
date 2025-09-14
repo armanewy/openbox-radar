aeb6f312c84be5af4ccd3d7ad899867b9c147097
Arman Ozturk <aozturk6@gmail.com>
2025-09-14 13:35:14 -0400

    Add DB url env variable

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "drizzle-kit";
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  dialect: "postgresql",
-  schema: "./lib/drizzle/schema.ts",
-  out: "./drizzle",
+  dialect: 'postgresql',
+  schema: './lib/drizzle/schema.ts',
+  out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
