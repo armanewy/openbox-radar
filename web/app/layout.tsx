f2c0d5757f7866cfbe79dea9da5ba2860e7b8d0b
Arman Ozturk <aozturk6@gmail.com>
2025-09-20 16:51:34 -0400

    update favicons again

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -13,10 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   const s = getSession();
   return (
     <html lang="en">
-       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <head>
+        <link rel="icon" href="/favicon-small.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#0f9d58" />
